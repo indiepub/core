bb78fa736358f8dc727f7d957b08fefd728ce0f6
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-02-08 07:56:59 -0600

    feat: :fire: removes the site config option because it was too opinionated

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,11 +1,7 @@
 import { z } from "zod"
-import { safeDate, safeUrl } from "../utils"
+import { safeDate } from "../utils"
 
-export type SchemaOptions = {
-	site: string
-}
-
-export function createSchemas(options: SchemaOptions) {
+export function createSchemas() {
 	const baseSchema = z.object({
 		published: safeDate("when the entry was published").optional(),
 		updated: safeDate("when the entry was updated").optional(),
@@ -16,7 +12,8 @@ export function createSchemas(options: SchemaOptions) {
 	const articleSchema = baseSchema.extend({
 		name: z.string().describe("entry name/title"),
 		summary: z.string().describe("short entry summary").optional(),
-		featured: safeUrl(options.site)
+		featured: z
+			.string()
 			.describe("primary photo for an article suitable for use in a link preview")
 			.optional(),
 	})
@@ -50,13 +47,12 @@ export function createSchemas(options: SchemaOptions) {
 		/* Draft properties */
 		photo: z
 			.string()
-			.or(safeUrl(options.site))
-			.or(z.string().or(z.array(safeUrl(options.site))))
+			.or(z.array(z.string()))
 			.describe("one or more photos that is/are considered the primary content of the entry")
 			.optional(),
-		video: safeUrl(options.site)
-			.or(z.string())
-			.or(z.string().or(z.array(safeUrl(options.site))))
+		video: z
+			.string()
+			.or(z.array(z.string()))
 			.describe("one or more videos that is/are considered the primary content of the entry")
 			.optional(),
 	})
@@ -64,7 +60,7 @@ export function createSchemas(options: SchemaOptions) {
 	const photoSchema = baseSchema.extend({
 		name: z.string().describe("caption of the photo, often used for figure captions"),
 		summary: z.string().describe("description of the photo, often used for alt text").optional(),
-		photo: safeUrl(options.site).describe("src URL for the original image file"),
+		photo: z.string().describe("src URL for the original image file"),
 	})
 
 	const personSchema = z.object({
@@ -73,10 +69,13 @@ export function createSchemas(options: SchemaOptions) {
 		givenName: z.string().describe("given (often first) name").optional(),
 		familyName: z.string().describe("family (often last) name").optional(),
 		email: z.string().email().describe("email address").optional(),
-		logo: safeUrl(options.site)
+		logo: z
+			.string()
 			.describe("a logo representing the person or organization, e.g. avatar icon")
 			.optional(),
-		url: safeUrl(options.site)
+		url: z
+			.string()
+			.url()
 			.describe("home page or other URL representing the person or organization")
 			.optional(),
 	})
